bfa7538e72dce2c9d9041f5b8f3227d7a33282ef
yum <yumzi114@gmail.com>
2024-09-19 14:59:31 +0900

    d

--- slave_bin/src/main.rs
+++ slave_bin/src/main.rs
@@ -4,157 +4,106 @@ use serde_derive;
 use serde_hex;
 use defaults::Defaults;
 use serde_hex::{SerHex,StrictPfx};
-use tokio::{self};
+use tokio::{self, task};
 use mini_redis::{client, Result};
 use core::result::Result as ResultC;
-use std::{thread, time::Duration};
+use tokio::runtime::Runtime;
+use std::{sync::{Arc, Mutex}, thread, time::Duration};
 use futures::{stream::StreamExt, SinkExt};
 use futures_channel::mpsc;
 use tracing::{info, trace, warn, error};
 use tokio_serial::{SerialPort, SerialPortBuilderExt, StopBits};
 use tokio_util::codec::{Decoder, Encoder};
-use slaveapi::{self, LineCodec};
+use slaveapi::{self, LineCodec, Packet};
+use rppal::gpio::Gpio;
 #[cfg(unix)]
 // const SERIAL_DEVICE: &'static str = env!("SERIAL_DEVICE");
 const SERIAL_DEVICE: &'static str = "/dev/ttyAMA2";
 // const SERIAL_DEVICE : "/dev/ttyACM1";
 
-#[derive(Debug,PartialEq,Eq,Serialize,Deserialize,Defaults,Clone,Copy)]
-struct Responese{
-    #[serde(with = "SerHex::<StrictPfx>")]
-    #[def = "0xAF"]
-    start: u8,
-    #[serde(with = "SerHex::<StrictPfx>")]
-    #[def = "0x03"]
-    length: u8,
-    #[serde(with = "SerHex::<StrictPfx>")]
-    #[def = "0x00"]
-    command: u8,
-    #[serde(with = "SerHex::<StrictPfx>")]
-    #[def = "0x00"]
-    data: u8,
-    #[serde(with = "SerHex::<StrictPfx>")]
-    #[def = "0x00"]
-    checksum: u8,
-    #[serde(with = "SerHex::<StrictPfx>")]
-    #[def = "0xFC"]
-    end: u8,
-}
-impl Responese {
-    fn parser (&mut self, buf : &Vec<u8>)->ResultC<(),String>{
-        if buf.len()==6{
-            // let mut resp=Responese::default();
-            self.start = u8::from_be_bytes([buf[0]]);
-            self.length = u8::from_be_bytes([buf[1]]);
-            self.command = u8::from_be_bytes([buf[2]]);
-            self.data = u8::from_be_bytes([buf[3]]);
-            self.checksum = u8::from_be_bytes([buf[4]]);
-            self.end = u8::from_be_bytes([buf[5]]);
-            return Ok(())
-        }
-        else {
-            return Err("Fail Parsing".to_string())
-        }
-    }
-    fn is_checksum(&self)->ResultC<(),String>{
-        let mut sumdata:u128=0;
-        // for i in list[1..3].iter() {
-        //     let num = *i;
-        //     num.to_be_bytes().map(|x|sumdata+=u128::from(x));
-        // }
-        self.length.to_be_bytes().map(|x|sumdata+=u128::from(x));
-        self.command.to_be_bytes().map(|x|sumdata+=u128::from(x));
-        self.data.to_be_bytes().map(|x|sumdata+=u128::from(x));
 
-        let hex_str = format!("{:#x}",sumdata);
-        let check_sum =hex::decode(&hex_str[hex_str.len()-2..]);
-        if let Ok(data)=check_sum{
-            if self.checksum!=data[0]{
-                return Err("Fail checksum Err".to_string());
-            }
-            let num = data[0].to_string();
-            return Ok(());
-        }
-        else{
-            let hex_str = hex_str.trim_start_matches("0x");
-            let checksum=u8::from_str_radix(hex_str,16).unwrap();
-            if self.checksum!=checksum{
-                return Err("Fail checksum Err".to_string());
-            }
-            let num = checksum.to_string();
-            return Ok(());
-        }
-    }
-    fn to_vec(&self)->ResultC<Vec<u8>,String>{
-        let mut list = vec![];
-        list.push(self.start);
-        list.push(self.length);
-        list.push(self.command);
-        list.push(self.data);
-        list.push(self.checksum);
-        list.push(self.end);
-        Ok(list)
-    }
-}
+const UP_BT: u8 = 2;
+const DOWN_BT: u8 = 3;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::new()
     ).expect("setting default subscriber failed");
+    let up_pin = Arc::new(Mutex::new(Gpio::new().unwrap().get(UP_BT).unwrap().into_input_pullup()));
+    let down_pin = Arc::new(Mutex::new(Gpio::new().unwrap().get(DOWN_BT).unwrap().into_input_pullup()));
+    // let mut pin = Gpio::new().unwrap().get(DOWN_BT)?.into_output();
+    
+    //=============시리얼설정=============
     let mut port = tokio_serial::new(SERIAL_DEVICE, 115200).open_native_async().unwrap();
     #[cfg(unix)]
     port.set_stop_bits(StopBits::One).unwrap();
+    
     let (mut writer, mut reader) = LineCodec.framed(port).split();
-    let serial_handle = tokio::task::Builder::new()
-        .name("Serial Reciver")
-        .spawn(async move{
-            // let mut port = tokio_serial::new(SERIAL_DEVICE, 115200).open_native_async().unwrap();
-            #[cfg(unix)]
-            // port.set_stop_bits(StopBits::One).unwrap();
-            // let mut reader =LineCodec.framed(port);
+    //=============시리얼 수신 스레드=============
+    
+    thread::spawn(move||{
+        let rt  = Runtime::new().unwrap();
+        rt.block_on(async {
             while let Some(line_result) = reader.next().await {
                 if let Ok(packet)=line_result{
                     match packet.command{
-                        0x01=>info!("READ [Request]: {:?}",packet),
+                        0x01=>info!("READ [REQUEST]: {:?}",packet),
                         0x02=>info!("READ [RESPONSE]: {:?}",packet),
                         0x03=>info!("READ [REPORT]: {:?}",packet),
                         _=>{}
                     }
                     
                 }
-                // let mut respon = Responese::default();
-                // if let Ok(line)=line_result{
-                //     if let Ok(_)=respon.parser(&line){
-                //         match respon.is_checksum() {
-                //             Ok(_)=>println!("{:?}", respon),
-                //             Err(_)=>println!("CheckSum Error"),
-                //         }
-                //         // println!("{:?}", respon);    
-                //     }
-                // }
             }
         });
-
+            
+        });
+    //=============시리얼 송신 스레드=============
+    thread::spawn(move||{
+        let rt  = Runtime::new().unwrap();
+        rt.block_on(async {
+            loop{
+                if up_pin.lock().unwrap().is_low(){
+                    let mut req = Packet::default();
+                    req.command = 0x01;
+                    req.remote=0b0000_0001;
+                    if let Ok(_)=req.add_checksum(){
+                        if let Ok(_)=req.is_checksum(){
+                            if let Ok(_)=writer.send(req).await{
+                                info!("SEND [REQUEST]: {:?}",req);
+                            }
+                        }
+                    }
+                }
+                if down_pin.lock().unwrap().is_low(){
+                    let mut req = Packet::default();
+                    req.command = 0x01;
+                    req.remote=0b0000_0010;
+                    if let Ok(_)=req.add_checksum(){
+                        if let Ok(_)=req.is_checksum(){
+                            if let Ok(_)=writer.send(req).await{
+                                info!("SEND [REQUEST]: {:?}",req);
+                            }
+                        }
+                    }
+                }
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+        
+    });
     loop{
+        
         // println!("Main Loop");
-        thread::sleep(Duration::from_millis(1000));
-        // let test = Responese::default();
-        // if let Ok(list) = test.to_vec(){
-        //     if let Ok(send_data)=writer.send(list).await{
-        //         println!("SEND : {:?}",test);
-        //     }
+        
+        // if up_pin.is_low(){
+        //     info!("UP BOTTON IS CLICKED");
         // }
+        // if down_pin.is_low(){
+        //     info!("DOWN BOTTON IS CLICKED");
+        // }
+        thread::sleep(Duration::from_millis(50));
     }
-
-    // let mut port = tokio_serial::new(SERIAL_DEVICE, 115200).open_native_async().unwrap();
-    // #[cfg(unix)]
-    // port.set_stop_bits(StopBits::One).unwrap();
-    // let (writer, mut reader) = LineCodec.framed(port).split();
-    // while let Some(line_result) = reader.next().await {
-    //     let line = line_result.expect("Failed to read line");
-    //     println!("{}", line);
-    // }
-    
     Ok(())
 }
--- slaveapi/src/lib.rs
+++ slaveapi/src/lib.rs
@@ -13,8 +13,6 @@ use defaults::Defaults;
 const SERIAL_DEVICE: &'static str = "/dev/ttyAMA2";
 #[cfg(windows)]
 const DEFAULT_TTY: &str = "COM1";
-
-
 pub struct LineCodec;
 
 impl Decoder for LineCodec {
@@ -27,44 +25,38 @@ impl Decoder for LineCodec {
         if let Some(n) = start {
             let line = src.split_to(n+1);
             let line_list = line.to_vec();
-            if line_list.len()==14&&line_list[0]==0xAF&&line_list[1]==11{
+            if line_list.len()==15&&line_list[0]==0xAF&&line_list[1]==12{
                 let mut packet = Packet::default();
                 if let Ok(_)=packet.parser(line_list){
                     if let Ok(_)=packet.is_checksum(){
                         return Ok(Some(packet));
                     }
                 }
-                // if line_list[3]==00{
-                //     return Err(Error::other("Device S/N Error"));
-                // }
-                // return Ok(Some(line_list));
             }
             else {
                 return Ok(None)
-                // return Err(Error::new(ErrorKind::NotConnected, "Device Not Connected"));
             }
 
         }
-        // if let Some(n) = newline {
-        //     let line = src.split_to(n + 1);
-        //     return match str::from_utf8(line.as_ref()) {
-        //         Ok(s) => Ok(Some(s.to_string())),
-        //         Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid String")),
-        //     };
-        // }
         Ok(None)
     }
 }
 
-impl Encoder<Vec<u8>> for LineCodec {
+impl Encoder<Packet> for LineCodec {
     type Error = io::Error;
 
-    fn encode(&mut self, item: Vec<u8>, buf: &mut BytesMut) -> Result<(), Self::Error> {
-        
-        for i in item{
-            buf.put_u8(i);
-        }
-        
+    fn encode(&mut self, item: Packet, buf: &mut BytesMut) -> Result<(), Self::Error> {
+        buf.put_u8(item.start);
+        buf.put_u8(item.length);
+        buf.put_u16(item.reserved);
+        buf.put_u8(item.command);
+        buf.put_u8(item.remote);
+        buf.put_i16(item.pannel_up);
+        buf.put_i16(item.pannel_down);
+        buf.put_i16(item.overload);
+        buf.put_u8(item.sensor_state);
+        buf.put_u8(item.checksum);
+        buf.put_u8(item.end);
         Ok(())
     }
 }
@@ -73,12 +65,14 @@ impl Encoder<Vec<u8>> for LineCodec {
 pub struct Packet {
     #[def = "0xAF"]
     start: u8,
-    #[def = "0x0B"]
+    #[def = "0x0C"]
     length: u8,
     #[def = "0x0000"]
     reserved: u16,
     #[def = "0x00"]
     pub command: u8,
+    #[def = "0x00"]
+    pub remote: u8,
     #[def = "0x0000"]
     pub pannel_up: i16,
     #[def = "0x0000"]
@@ -94,17 +88,18 @@ pub struct Packet {
 }
 impl Packet{
     pub fn parser(&mut self, buf:Vec<u8>)->Result<(),io::Error>{
-        if buf.len()==14{
+        if buf.len()==15{
             self.start = buf[0];
             self.length = u8::from_be_bytes([buf[1]]);
             self.reserved = u16::from_be_bytes([buf[2],buf[3]]);
             self.command = u8::from_be_bytes([buf[4]]);
-            self.pannel_up = i16::from_be_bytes([buf[5],buf[6]]);
-            self.pannel_down = i16::from_be_bytes([buf[7],buf[8]]);
-            self.overload = i16::from_be_bytes([buf[9],buf[10]]);
-            self.sensor_state = u8::from_be_bytes([buf[11]]);
-            self.checksum = u8::from_be_bytes([buf[12]]);
-            self.end = u8::from_be_bytes([buf[13]]);
+            self.remote = u8::from_be_bytes([buf[5]]);
+            self.pannel_up = i16::from_be_bytes([buf[6],buf[7]]);
+            self.pannel_down = i16::from_be_bytes([buf[8],buf[9]]);
+            self.overload = i16::from_be_bytes([buf[10],buf[11]]);
+            self.sensor_state = u8::from_be_bytes([buf[12]]);
+            self.checksum = u8::from_be_bytes([buf[13]]);
+            self.end = u8::from_be_bytes([buf[14]]);
 
             return Ok(())    
         }
@@ -116,6 +111,7 @@ impl Packet{
         let mut sumdata:u128=0;
         self.reserved.to_be_bytes().map(|x|sumdata+=u128::from(x));
         self.command.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.remote.to_be_bytes().map(|x|sumdata+=u128::from(x));
         self.pannel_up.to_be_bytes().map(|x|sumdata+=u128::from(x));
         self.pannel_down.to_be_bytes().map(|x|sumdata+=u128::from(x));
         self.overload.to_be_bytes().map(|x|sumdata+=u128::from(x));
@@ -134,6 +130,28 @@ impl Packet{
             if self.checksum!=checksum{
                 return Err("Fail checksum Err".to_string());
             }
+            return Ok(());
+        }
+    }
+    pub fn add_checksum (&mut self)->Result<(),String>{
+        let mut sumdata:u128=0;
+        self.reserved.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.command.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.remote.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.pannel_up.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.pannel_down.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.overload.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.sensor_state.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        let hex_str = format!("{:#x}",sumdata);
+        let check_sum =hex::decode(&hex_str[hex_str.len()-2..]);
+        if let Ok(data)=check_sum{
+            self.checksum = data[0];
+            return Ok(());
+        }
+        else{
+            let hex_str = hex_str.trim_start_matches("0x");
+            let checksum=u8::from_str_radix(hex_str,16).unwrap();
+            self.checksum = checksum;
             return Ok(());
         }
     }
